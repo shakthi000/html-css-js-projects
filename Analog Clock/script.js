a52552f5cda12af2612a711205593d3ec0b812da
Shakthi Akshata <144229462+shakthi000@users.noreply.github.com>
2024-06-08 00:27:29 +0530

    Add files via upload

---
+++ Analog Clock/script.js
@@ -0,0 +1,46 @@
+// Get references to DOM elements
+const body = document.querySelector("body"),
+    hrHand = document.querySelector(".hr"),
+    minHand = document.querySelector(".min"),
+    secHand = document.querySelector(".sec"),
+    modeSwitch = document.querySelector(".mode-switch");
+
+// check if the mode is already set to "Dark Mode" in localStorage
+if(localStorage.getItem("mode") === "Dark Mode") {
+    // add "dark" class to body and set modeSwitch text to "Light Mode"
+    body.classList.add("dark");
+    modeSwitch.textContent = "Light Mode";
+}
+
+    // Add a click event listener to modeSwitch
+    modeSwitch.addEventListener("click",()=>{
+        // toggle the "dark" class on the body element
+        body.classList.toggle("dark");
+        // check if the "dark" class is currently present on the body element 
+        const isDarkMode = body.classList.contains("dark");
+        // set modeSwitch text based on "dark" class presence
+        modeSwitch.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
+
+        // set localStorage "mode" item based on "dark" class presence
+        localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode");
+    });
+
+    const updateTime = ()=> {
+        // Get current time and calculate degrees for clock hands
+        let date = new Date(),
+        secToDeg = (date.getSeconds() / 60) * 360,
+        minToDeg = (date.getMinutes() / 60) * 360,
+        hrToDeg = (date.getHours() / 12) * 360;
+
+        // Rotate the clock hands to appropriate degree based on current time
+        secHand.style.transform = `rotate(${secToDeg}deg)`,
+        minHand.style.transform = `rotate(${minToDeg}deg)`,
+        hrHand.style.transform = `rotate(${hrToDeg}deg)`;
+    };
+
+
+// Call updateTime to set clock hands to every second
+setInterval(updateTime, 1000);
+
+// Call updateTime function on page load
+updateTime();
